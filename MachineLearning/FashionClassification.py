770d2128e65be400eb58315541d9284bff59fea8
acao2002 <63988697+acao2002@users.noreply.github.com>
2020-06-11 05:03:05 -0400

    Add files via upload

---
+++ MachineLearning/FashionClassification.py
@@ -0,0 +1,38 @@
+import tensorflow as tf 
+from tensorflow import keras 
+import matplotlib.pyplot as plt
+import numpy as np 
+
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+result = np.argmax(predictions[0])
+
+print(result)
+print(class_names[result])
+print(class_names [test_labels[0]])
