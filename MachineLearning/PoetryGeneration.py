0df31e51af6e1034d5d220ebc891acfbecc24dea
acao2002 <63988697+acao2002@users.noreply.github.com>
2020-06-27 02:59:49 -0400

    added Poetry Generator

---
+++ MachineLearning/PoetryGeneration.py
@@ -0,0 +1,43 @@
+import tensorflow as tf
+
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.preprocessing.text import Tokenizer
+import numpy as np 
+from tensorflow import keras 
+
+tokenizer = Tokenizer()
+
+data = open('irish-lyrics-eof.txt').read()
+
+corpus = data.lower().split("\n")
+
+tokenizer.fit_on_texts(corpus)
+total_words = len(tokenizer.word_index) + 1
+
+input_sequences = []
+for line in corpus:
+	token_list = tokenizer.texts_to_sequences([line])[0]
+	for i in range(1, len(token_list)):
+		n_gram_sequence = token_list[:i+1]
+		input_sequences.append(n_gram_sequence)
+
+# pad sequences 
+max_sequence_len = max([len(x) for x in input_sequences])
+
+model = keras.models.load_model('poetryModel.h5')
+
+seed_text = "I love you"
+next_words = 10
+  
+for _ in range(next_words):
+	token_list = tokenizer.texts_to_sequences([seed_text])[0]
+	token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
+	predicted = np.argmax(model.predict(token_list))
+	output_word = ""
+	for word, index in tokenizer.word_index.items():
+		if index == predicted:
+			output_word = word
+			break
+	seed_text += " " + output_word
+print(seed_text)
+print(max_sequence_len)
